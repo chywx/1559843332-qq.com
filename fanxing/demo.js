9df954f28adfaa18ab958db5013901b6ab8c53e6
陈海洋 <1559843332@qq.com>
2020-04-30 17:37:55 +0800

    泛型学习

--- fanxing/demo.js
+++ fanxing/demo.js
@@ -1,6 +1,6 @@
-var getData = function (value) {
+var ConfigFnImpl = function (value) {
     return value;
 };
+var getData = ConfigFnImpl;
 getData('张三');
 // getData<string>(1243);  //错误
-getData(123);
--- fanxing/demo.ts
+++ fanxing/demo.ts
@@ -1,14 +1,16 @@
 /**
  * 3 泛型函数接口
  */
-interface ConfigFn{
-    <T>(value:T):T;
+interface ConfigFn {
+    <T>(value: T): T;
 }
 
-var getData:ConfigFn=function<T>(value:T):T{
+var ConfigFnImpl = function <T>(value: T): T {
     return value;
 }
+
+var getData: ConfigFn = ConfigFnImpl
+
 getData<string>('张三');
 // getData<string>(1243);  //错误
-getData<number>(123);
 
